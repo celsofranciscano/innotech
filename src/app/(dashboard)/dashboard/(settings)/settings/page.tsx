c622b549b8e8b969c932503aa356ea57d69d16fe
Celso Franciscano <162634097+celsofranciscano@users.noreply.github.com>
2025-09-02 08:15:51 -0400

    Actualizar page.tsx delete etiqueta a

--- src/app/(dashboard)/dashboard/(settings)/settings/page.tsx
+++ src/app/(dashboard)/dashboard/(settings)/settings/page.tsx
@@ -3,18 +3,7 @@ function SettingsPage() {
         <main>
             Pagina de configuraciones
 
-            <a href="/dashboard/settings/privileges">
-                <button>Ver privilegios</button>
-            </a>
-
-            <a href="/dashboard/settings/users">
-                <button>Ver usuarios</button>
-            </a>
-
-            <a href="/dashboard/settings/roles">
-                <button>Ver roles</button>
-            </a>
-
+           
         </main>
      );
 }
